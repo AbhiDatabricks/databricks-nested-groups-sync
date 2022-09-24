b2b35ed79bd6935ac27082319e609197ca823983
aps <abhishekpratap.singh@databricks.com>
2022-09-25 00:41:33 +1000

    locals

--- fetch_groups.tf
+++ fetch_groups.tf
@@ -2,3 +2,8 @@ data "azuread_group" "this" {
   for_each = local.all_groups
   display_name = each.value
 }
+
+locals {
+  all_groups = toset(keys(var.groups))
+  admin_groups = toset([for k,v in var.groups: k if v.admin])
+}
