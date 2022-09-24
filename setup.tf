f11f1f7fff25df69bd870b3f6a63204094ac3b41
aps <abhishekpratap.singh@databricks.com>
2022-09-25 01:03:00 +1000

    cleanup

--- setup.tf
+++ setup.tf
@@ -5,12 +5,6 @@ variable "region" {
 
 
 
-resource "random_string" "suffix" {
-  special = false
-  upper   = false
-  length  = 6
-}
-
 locals {
   groupname = "mygroup${random_string.suffix.result}"
 }
