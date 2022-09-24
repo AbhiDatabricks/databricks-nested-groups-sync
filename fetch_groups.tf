b9321faadd9562096d158d15f69338a4b809dc7f
aps <abhishekpratap.singh@databricks.com>
2022-09-25 00:52:45 +1000

    return

--- fetch_groups.tf
+++ fetch_groups.tf
@@ -1,20 +1,23 @@
-data "azuread_group" "this" {
-  for_each = local.all_groups
-  display_name = each.value
+data "azuread_groups" "all" {
+  return_all = true
 }
+# data "azuread_group" "this" {
+#   for_each = local.all_groups
+#   display_name = each.value
+# }
 
-locals {
-  all_groups = toset(keys(var.groups))
-  admin_groups = toset([for k,v in var.groups: k if v.admin])
-}
+# locals {
+#   all_groups = toset(keys(var.groups))
+#   admin_groups = toset([for k,v in var.groups: k if v.admin])
+# }
 
-variable "groups" {
-  description = "Map of AAD group names into object describing workspace & Databricks SQL access permissions"
-  type = map(object({
-    workspace_access = bool
-    databricks_sql_access = bool
-    allow_cluster_create = bool
-    allow_instance_pool_create = bool
-    admin = bool  # if this group for Databricks admins
-  }))
-}
+# variable "groups" {
+#   description = "Map of AAD group names into object describing workspace & Databricks SQL access permissions"
+#   type = map(object({
+#     workspace_access = bool
+#     databricks_sql_access = bool
+#     allow_cluster_create = bool
+#     allow_instance_pool_create = bool
+#     admin = bool  # if this group for Databricks admins
+#   }))
+# }
